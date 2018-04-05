ce4027dcf7ed84794605aa3d3297627c5365b223
kriwsha <semon-ka@mail.ru>
2018-04-05 22:29:30 +0300

    consumer

--- src/main/java/bva/kafka/consumer/KafkaConsumer.java
+++ src/main/java/bva/kafka/consumer/KafkaConsumer.java
@@ -1,6 +1,7 @@
 package bva.kafka.consumer;
 
 import bva.kafka.lib.ConsumerService;
+import bva.kafka.lib.HandlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,10 @@ import org.springframework.stereotype.Component;
 public class KafkaConsumer implements ConsumerService {
 
     @Autowired
-
+    private HandlerService service;
 
     @Override
     public void start() {
-
+        service.handle("");
     }
 }
