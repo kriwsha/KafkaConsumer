2499a60faecf68083a8bac8e04ea042fef76610a
kriwsha <semon-ka@mail.ru>
2018-04-08 20:05:02 +0300

    consumer pool to refactor

--- src/main/java/bva/kafka/pool/KafkaConsumerPool.java
+++ src/main/java/bva/kafka/pool/KafkaConsumerPool.java
@@ -1,13 +1,16 @@
 package bva.kafka.pool;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 public class KafkaConsumerPool {
     private KafkaSettings settings;
     private int numberOfConsumers;
-    private CountDownLatch _latch =null;
-    private CancellationToken _cancelToken = new CancellationToken (  );
-    private DestinationFactory _factory;
-    private static final Logger logger = LogManager.getLogger(KafkaConsumerPool.class);
-    private Map<Integer,KafkaWorker> _workers = new  HashMap<Integer,KafkaWorker>();
+    private CountDownLatch latch;
+    private CancellationToken cancelToken = new CancellationToken();
+    private DestinationFactory factory;
+    private Map<Integer,KafkaWorker> workers = new HashMap<>();
 
     public KafkaConsumerPool (int numberOfConsumers, KafkaSettings settings, DestinationFactory factory, String tlvLibraryPath) {
         this.settings = settings;
@@ -15,49 +18,42 @@ public class KafkaConsumerPool {
             throw new IllegalArgumentException("В конфигурационном файле для Kafka не указана тема для подписки!");
         }
         this.numberOfConsumers = numberOfConsumers;
-        this._factory = factory;
+        this.factory = factory;
         // Инициализируем библиотеку конфигурации
         TlvLibrary.init(tlvLibraryPath);
     }
 
-    public void execute() throws KktImporterException, DestinationCreateException {
+    public void execute() throws Exception{
         try {
-            _latch = new CountDownLatch ( numberOfConsumers ); // Синхронизация потоков
-            for (int i = 0; i < numberOfConsumers; i++) {
-                KafkaWorker w = createConsumerWorker ( );
-                logger.info ( "Запущен поток " + w.getName ( ) + " " + w.getThreadGroup ( ) + " " + w.getId ( ) );
-                _workers.put ( i, w );
-            }
-            for (KafkaWorker w : _workers.values ( )) {
-                w.start ( );
-            }
+            latch = new CountDownLatch (numberOfConsumers); // Синхронизация потоков
+            for (int i = 0; i < numberOfConsumers; i++)
+                workers.put (i, createConsumerWorker());
+            for (KafkaWorker w : workers.values())
+                w.start();
         }
         catch (Exception e){
-            _cancelToken.closeAll ();
-            logger.error ( e );
+            cancelToken.closeAll();
             throw e;
         }
         finally{
             // Ожидаем остановки всех потомков
             try {
-                _latch.await ();
+                latch.await ();
                 // Если есть ошибка, прокидываем ее выше
-                if (_cancelToken.hasException ())
-                    throw new KktImporterException ( _cancelToken.getLastException () );
+                if (cancelToken.hasException ())
+                    throw new Exception (cancelToken.getLastException());
             } catch (InterruptedException e) {
                 e.printStackTrace ( );
             }
         }
     }
 
-    @Override
     public void stop() {
-        _workers.values ().forEach ( w->w.shutdown () );
+        workers.values().forEach(KafkaWorker::shutdown);
         try {
-            _latch.await ();
-        } catch (Exception e) {
-            e.printStackTrace ( );
-            logger.fatal ( "Прерывание в главном потоке" );
+            latch.await ();
+        } catch (Exception ex) {
+            ex.printStackTrace ( );
         }
     }
 
--- src/main/java/bva/kafka/pool/OffsetStorage.java
+++ src/main/java/bva/kafka/pool/OffsetStorage.java
@@ -3,7 +3,7 @@ package bva.kafka.pool;
 import org.apache.kafka.common.TopicPartition;
 
 public interface OffsetStorage {
-    void commitPosition (TopicPartition partition, long position) throws WrongOffsetException, OffsetStorageException;
+    void commitPosition (TopicPartition partition, long position) throws OffsetStorageException;
     long getPosition(TopicPartition partition) throws OffsetStorageException;
     void close();
 }
