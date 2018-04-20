027969abb7e8e13c3c902496cf477d9c0ee18375
kriwsha <semon-ka@mail.ru>
2018-04-20 22:43:49 +0300

    offset storage

--- src/main/java/bva/kafka/lib/OffsetStorage.java
+++ src/main/java/bva/kafka/lib/OffsetStorage.java
@@ -3,7 +3,7 @@ package bva.kafka.lib;
 import org.apache.kafka.common.TopicPartition;
 
 public interface OffsetStorage {
-    void commitOffset(TopicPartition partition, long position);
-    long getOffset();
+    void commitOffset(TopicPartition partition, long position) throws Exception;// TODO: 20.04.2018 поправить вид исключения
+    long getOffset(String path) throws Exception;// TODO: 20.04.2018 поправить вид исключения
     void close() throws Exception;
 }
--- src/main/java/bva/kafka/lib/ZookeeperOffsetStorage.java
+++ src/main/java/bva/kafka/lib/ZookeeperOffsetStorage.java
@@ -1,10 +1,13 @@
 package bva.kafka.lib;
 
 import org.apache.kafka.common.TopicPartition;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ZookeeperOffsetStorage implements OffsetStorage{
     private ZooKeeper zk;
@@ -20,20 +23,42 @@ public class ZookeeperOffsetStorage implements OffsetStorage{
     }
 
     @Override
-    public void commitOffset(TopicPartition partition, long position) {
+    public void commitOffset(TopicPartition partition, long position) throws Exception {// TODO: 20.04.2018 поправить исключение здесь и в интерфейсе
         String fullPath = createFullPath(partition.topic(), partition.partition());
-        byte[] positionByteView = String.valueOf(position).getBytes();
-
+        try {
+            long previousPosition = getOffset(fullPath);
+            if (previousPosition < position) {
+                updatePosition(path, position);
+            } else {
+                throw new Exception(String.format("Bad offset in: %s, current: %d, new: %d", fullPath, position, previousPosition)); // TODO: 20.04.2018 изменить на WrongOffsetException
+            }
+        } catch (KeeperException ex) {
+            throw new Exception(); // TODO: 20.04.2018 изменить на OffsetStorageException
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();// TODO: 20.04.2018 change to logging
+        }
     }
 
     @Override
-    public long getOffset() {
-        return 0;
+    public long getOffset(String fullPath) throws Exception {// TODO: 20.04.2018 поправить
+        try {
+            Stat existStat = zk.exists(fullPath, true);
+            byte[] bytesPosition = zk.getData(fullPath, true, existStat);
+            return Long.parseLong(new String(bytesPosition, StandardCharsets.UTF_8));
+        } catch (KeeperException | InterruptedException ex) {
+            throw new Exception();  // TODO: 20.04.2018 добавить исключение OffsetStorageException
+        }
     }
 
     @Override
     public void close() throws InterruptedException {
         zk.close();
+    }
+
+    private void updatePosition(String path, long position) throws KeeperException, InterruptedException{
+        byte[] currentPositionByteView = String.valueOf(position).getBytes();
+        int version = zk.exists(path, true).getVersion();
+        zk.setData(path, currentPositionByteView, version);
     }
 
     private String createFullPath(String topik, int partition) {
