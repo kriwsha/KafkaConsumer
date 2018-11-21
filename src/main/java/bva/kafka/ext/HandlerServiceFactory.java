874707cedd143e492f540f2ecd19944c03434875
vladik <belov_v.a@mail.ru>
2018-11-21 21:50:42 +0300

    Отладил serviceFactory - теперь нет исключения

--- src/main/java/bva/kafka/ext/HandlerServiceFactory.java
+++ src/main/java/bva/kafka/ext/HandlerServiceFactory.java
@@ -3,9 +3,14 @@ package bva.kafka.ext;
 import bva.kafka.lib.HandlerService;
 import bva.kafka.lib.ServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
+@Service
+@Lazy
 public class HandlerServiceFactory implements ServiceFactory {
     @Autowired
     HandlerService[] services;
